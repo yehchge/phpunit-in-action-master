26c63b212819a4fe1badb0aa88d9dcc2a2c5bc84
yehchge <yehchge@gmail.com>
2018-07-17 11:47:06 +0800

    加入 Email 檢查

---
+++ EmailTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers Email
+ */
+final class EmailTest extends TestCase {
+
+    public function testCanVeCreatedFromValidEmailAddress(){
+        $this->assertInstanceOf(
+            Email::class,
+            Email::fromString('user@example.com')
+        );
+    }
+
+    public function testCannotBeCreatedFromInvalidEmailAddress(){
+        $this->expectException(InvalidArgumentException::class);
+        Email::fromString('invalid');
+    }
+
+    public function testCanBeUsedAsString(){
+        $this->assertEquals(
+            'user@example.com',
+            Email::fromString('user@example.com')
+        );
+    }
+
+}
